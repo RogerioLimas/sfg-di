4d9363068f16a771cd68fde7c933142b1f789dbc
Rogério Limas Moreira <rogerio.moreira@visto.global>
2020-10-01 19:25:18 -0300

    Reviewing Profiles

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by jt on 12/28/19.
  */
 @Profile({"dog", "default"})
-public class DogPetService {
+@Service
+public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
     }
